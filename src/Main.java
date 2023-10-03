d1a16bb2f055906f1150934e66fdb840e0c9392f
nek07 <abylaiooaooa@mail.ru>
2023-10-03 21:19:28 +0600

    singleton's getInstance

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        Cat boris = new Cat();
+        Cat boris = Cat.getInstance();
+        Cat moris = Cat.getInstance();
         boris.setBehavior(new MeowBehavior());
         boris.performBehavior();
         boris.setBehavior(new EatBehavior());
         boris.performBehavior();
         boris.setBehavior(new ScratchBehavior());
         boris.performBehavior();
+
+        System.out.println(moris==boris); //if it is true, that means the class has only one instance
     }
 }
